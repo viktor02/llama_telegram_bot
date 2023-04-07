be976fd34a646be35cd3eda4612937b402dcd6cd
Viktor Karpov <v@vitka-k.ru>
2023-04-07 15:26:11 +0300

    removed streaming mode as buggy; some fixes

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ logger.info(f"Seed: {seed}")
 historyDb = ChatHistoryDB("chat.db")
 
 
-def send_by_chunks(bot, message, text, **kwargs):
+def send_by_chunks(message, text, **kwargs):
     if len(text) <= 4096:
         bot.reply_to(message, text, **kwargs)
     else:
@@ -47,9 +47,8 @@ def send_by_chunks(bot, message, text, **kwargs):
 def get_last_messages(chat_id):
     messages = historyDb.get_chat_messages(chat_id)
     history = ""
-    for msg in reversed(messages):
-        user_prompt, answer = msg
-        history += f"### Human: {user_prompt}\n### Assistant: {answer}\n"
+    for user_prompt, answer in messages:
+        history += f"### Human: {user_prompt}\n### Assistant: {answer}"
     return history
 
 
@@ -58,7 +57,7 @@ def generate_text(user_prompt, max_tokens=args.max_token, stream=False, custom_p
     prompt = f"### Human: {user_prompt}\n### Assistant:"
     if custom_prompt is not None:
         prompt = custom_prompt
-    if history is True:
+    if history is True and chat_id is not None:
         prompt = get_last_messages(chat_id) + prompt
 
     logger.info(f"Generation for text: {user_prompt}")
@@ -85,12 +84,15 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: call.data == 'remove_history')
 def send_message_callback(call):
     historyDb.delete_all_history(call.message.chat.id)
-    bot.send_message(call.message.chat.id, "History deleted succesfully")
+    bot.send_message(call.message.chat.id, "History deleted successfully")
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start_command(message):
-    bot.reply_to(message, "Hello.")
+    bot.reply_to(message, "Hello. This is chatGPT bot based on LLaMa.\nUsage:\n\n<text> - Q&A mode.\n"
+                          "/raw <prompt> - Use your own prompt.\n"
+                          "/history - show history and delete it\n\n"
+                          f"Current model: {model.name}")
 
 
 @bot.message_handler(commands=['raw'])
@@ -100,22 +102,26 @@ def stream_command(message):
     bot.reply_to(message, "Please wait a moment")
     bot.send_chat_action(chat_id=message.chat.id, action='typing')
     try:
-        json_obj = generate_text(user_prompt, max_tokens=256, stream=False, custom_prompt=user_prompt)
+        json_obj = generate_text(user_prompt, max_tokens=256, stream=False, chat_id=message.chat.id, history=True,
+                                 custom_prompt=user_prompt)
         output = json_obj['choices'][0]["text"]
 
         logger.debug(json.dumps(json_obj, indent=2))
         text_to_user = output
 
-        send_by_chunks(bot, message, text_to_user)
+        send_by_chunks(message, text_to_user)
     except OSError as e:
         bot.reply_to(message, f"OSError: {e}")
     except Exception as e:
         bot.reply_to(message, f"Error: {e}")
 
 
-@bot.message_handler(commands=['nostream'])
-def stream_command(message):
-    user_prompt = message.text.replace("/nostream ", '', 1)
+@bot.message_handler(func=lambda message: True)
+def main(message):
+    if message.text.startswith("/"):
+        bot.reply_to(message, "Wrong command")
+        return
+    user_prompt = message.text
     bot.reply_to(message, "Please wait a moment")
     bot.send_chat_action(chat_id=message.chat.id, action='typing')
     try:
@@ -125,48 +131,12 @@ def stream_command(message):
         logger.debug(json.dumps(json_obj, indent=2))
         text_to_user = output
 
-        send_by_chunks(bot, message, text_to_user)
+        send_by_chunks(message, text_to_user)
         historyDb.insert_message(message.chat.id, user_prompt, output)
     except OSError as e:
         bot.reply_to(message, f"OSError: {e}")
     except Exception as e:
         bot.reply_to(message, f"Error: {e}")
-
-
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    """ Streamed generation """
-    user_prompt = message.text
-    restricted_chars = ["", " ", "\n"]
-    try:
-        msg = bot.reply_to(message, "Please wait a moment")
-        bot.send_chat_action(chat_id=message.chat.id, action='typing')
-
-        stream = generate_text(user_prompt, stream=True, chat_id=message.chat.id, history=True)
-        output_buffer = ""
-
-        last_send_time = time.time()
-        send_interval = 2  # Every 2 seconds send message
-        for json_obj in stream:
-            logger.info(json.dumps(json_obj, indent=2))
-            output_buffer += json_obj['choices'][0]["text"]
-            if time.time() - last_send_time > send_interval or json_obj['choices'][0]["text"] in restricted_chars:
-                # fixed the 429 error from telegram - too fast editing
-                bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text=output_buffer)
-                last_send_time = time.time()
-            if len(output_buffer) > 4000:
-                # send a new message and clean buffer
-                msg = bot.reply_to(message, "Next part")
-                output_buffer = ""
-        try:
-            msg = bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text=output_buffer)
-        except telebot.apihelper.ApiTelegramException:
-            pass
-        historyDb.insert_message(message.chat.id, user_prompt, output_buffer)
-        logger.info(output_buffer)
-
-    except OSError as e:
-        bot.reply_to(message, f"OSError: {e}")
 
 
 bot.infinity_polling()
